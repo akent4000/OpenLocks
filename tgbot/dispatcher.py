44ac48c0578a7a105cf4bd55726c903a20a1083d
akent4000 <akent4000@yandex.ru>
2025-03-28 00:21:32 +0300

    test

--- tgbot/dispatcher.py
+++ tgbot/dispatcher.py
@@ -1,16 +1,16 @@
 
-import telebot
+# import telebot
 
-from tgbot.models import Configuration, TelegramBotToken
-from loguru import logger
+# from tgbot.models import Configuration, TelegramBotToken
+# from loguru import logger
 
-main_bot_token = TelegramBotToken.get_main_bot_token()
-test_bot_token = TelegramBotToken.get_test_bot_token()
+# main_bot_token = TelegramBotToken.get_main_bot_token()
+# test_bot_token = TelegramBotToken.get_test_bot_token()
 
-if Configuration.is_test_mode():
-    main_bot_token, test_bot_token = test_bot_token, main_bot_token
+# if Configuration.is_test_mode():
+#     main_bot_token, test_bot_token = test_bot_token, main_bot_token
 
-logger.add("logs/dispatcher.log", rotation="10 MB", level="INFO")
-bot = telebot.TeleBot(main_bot_token)
-test_bot = telebot.TeleBot(test_bot_token)
-logger.info("Bot instance created")
+# logger.add("logs/dispatcher.log", rotation="10 MB", level="INFO")
+# bot = telebot.TeleBot(main_bot_token)
+# test_bot = telebot.TeleBot(test_bot_token)
+# logger.info("Bot instance created")
