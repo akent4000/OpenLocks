860b9415d6ef82f87d9a9d551b9d48eba31ffb0f
akent4000 <40994229+akent4000@users.noreply.github.com>
2025-04-03 14:14:00 +0300

    add info for admins

---
+++ tgbot/logics/info_for_admins.py
@@ -0,0 +1,11 @@
+from tgbot.models import TelegramUser
+from tgbot.dispatcher import bot
+from telebot.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+
+def send_messege_to_admins(msg, markup=None, admins=None):
+    admins = admins if admins is not None else TelegramUser.objects.filter(send_admin_notifications=True)
+    for admins in admins:
+        try:
+            bot.send_message(admins.chat_id, msg, reply_markup=markup)
+        except:
+            pass
